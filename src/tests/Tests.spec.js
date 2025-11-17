ed91fd0a7282c75f81a3122492fe766052f5c0fc
edrikfsteiner <edrikfsteiner@gmail.com>
2025-11-17 19:34:16 -0300

    K6 test

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -8,43 +8,43 @@ export const getContactsDuration = new Trend('get_contacts', true);
 export const RateContentOK = new Rate('content_OK');
 
 export const options = {
-  thresholds: {
-    http_req_failed: ['rate<0.25'],
-    get_contacts: ['p(99)<500'],
-    content_OK: ['rate>0.75']
-  },
-  stages: [
-    { duration: '10s', target: 2 },
-    { duration: '10s', target: 4 },
-    { duration: '10s', target: 6 }
-  ]
+	thresholds: {
+		http_req_failed: ['rate<0.25'],
+		get_contacts: ['p(99)<500'],
+		content_OK: ['rate>0.75']
+	},
+	stages: [
+		{ duration: '10s', target: 2 },
+		{ duration: '10s', target: 4 },
+		{ duration: '10s', target: 6 }
+	]
 };
 
 export function handleSummary(data) {
-  return {
-    './src/output/index.html': htmlReport(data),
-    stdout: textSummary(data, { indent: ' ', enableColors: true })
-  };
+	return {
+		'./src/output/index.html': htmlReport(data),
+		stdout: textSummary(data, { indent: ' ', enableColors: true })
+	};
 }
 
 export default function () {
-  const baseUrl = 'https://reqres.in';
-  const endpoint = '/api/users?page=2';
+	const baseUrl = 'https://reqres.in';
+	const endpoint = '/api/users?page=2';
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+	const params = {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	};
 
-  const OK = 200;
+	const OK = 200;
 
-  const res = http.get(`${baseUrl}${endpoint}`, params);
+	const res = http.get(`${baseUrl}${endpoint}`, params);
 
-  getContactsDuration.add(res.timings.duration);
-  RateContentOK.add(res.status === OK);
+	getContactsDuration.add(res.timings.duration);
+	RateContentOK.add(res.status === OK);
 
-  check(res, {
-    'GET /api/users - Status 200': () => res.status === OK
-  });
+	check(res, {
+		'GET /api/users - Status 200': () => res.status === OK
+	});
 }
